6326d3090ccb31b7977da2c9d70b19a00c028fb7
Sezim Orozobekova <orozobekovasezim.kg@gmail.com>
2026-04-14 01:13:45 +0600

    feat(admin-dashboard): add pages for admin management

---
+++ src/app/admin-dashboard/admin-dashboard.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { AdminSidebar } from './components/admin-sidebar/admin-sidebar';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, AdminSidebar],
+  templateUrl: './admin-dashboard.html',
+  styleUrl: './admin-dashboard.css'
+})
+export class AdminDashboard {}
